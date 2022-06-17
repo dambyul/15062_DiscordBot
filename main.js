16eb6fe059f58be9db38b2d185af846085362901
Arrokoth <62744981+dambyul@users.noreply.github.com>
2022-06-17 17:29:59 +0900

    슬래시 커맨드 로딩 분리, 주석 조금

--- main.js
+++ main.js
@@ -7,33 +7,63 @@ const fs = require('node:fs');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+//작동 테스트
 client.once('ready', () => {
 	console.log(`client ready`)
 });
 
+//슬래시 커맨드
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Place your client and guild ids here
+//길드 아이디는 추후 정식 서비스 할 땐 사용하지 않을 예정 (테스트용)
 const clientId = '983640794126766112';
 const guildId = '983640497711087636';
 
+//commands 폴더에서 .js 파일 가져오기
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 const rest = new REST({ version: '9' }).setToken(token);
 
+//글로벌
+(async () => {
+	try {
+		await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: [] },
+		);
+		console.log('slash command reload - global');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+//단독 채널용
 (async () => {
 	try {
 		await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
-		console.log('slash command reload');
+		console.log('slash command reload - guild');
 	} catch (error) {
 		console.error(error);
 	}
 })();
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	console.log(interaction);
+
+	if (interaction.commandName === '모집') {
+		await interaction.reply('처리 완료');
+	}
+	
+	if (interaction.commandName === '군단장') {
+		await interaction.reply('처리 완료');
+	}
+});
 
 client.login(token);
