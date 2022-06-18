a01aba6c7d94566716d14af77c54f3eace9bcbb0
Arrokoth <62744981+dambyul@users.noreply.github.com>
2022-06-18 23:28:19 +0900

    뭔가 많이 수정함

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu   } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token } = require('./personal/token.json');
@@ -53,17 +52,276 @@ const rest = new REST({ version: '9' }).setToken(token);
 	}
 })();
 
+const contentsDic = {};
+const raidsDic = {};
+
+//카오스 던전
+contentsDic[1] = ['카오스 던전','https://cdn-lostark.game.onstove.com/uploadfiles/notice/17012ef4ba7d431daa0884121bb2d912.png'];
+//가디언 토벌
+contentsDic[2] = ['가디언 토벌','https://cdn-lostark.game.onstove.com/uploadfiles/notice/af2a2116f60441389eaa8a8db3624f26.png'];
+//어비스 던전
+contentsDic[3] = ['어비스 던전','https://cdn-lostark.game.onstove.com/uploadfiles/notice/a8d7c95daca24df0a33f561136de379a.png'];
+//어비스 레이드
+contentsDic[4] = ['어비스 레이드','https://cdn-lostark.game.onstove.com/uploadfiles/notice/8a845a8afd6347acb7ec1d468435419a.png'];
+//도가토
+contentsDic[5] = ['도전 가디언 토벌전','https://cdn-lostark.game.onstove.com/uploadfiles/notice/af2a2116f60441389eaa8a8db3624f26.png'];
+//도비스
+contentsDic[6] = ['도전 어비스 던전','https://ark.bynn.kr/assets/lostark/abyss_dungeon2.png'];
+//기타
+contentsDic[7] = ['기타','https://cdn-lostark.game.onstove.com/uploadfiles/notice/5f555811bd8443a8a8067466df3d4756.png'];
+//발탄
+raidsDic[1] = ['발탄(노말)','https://ark.bynn.kr/assets/lostark/commander0.png'];
+raidsDic[2] = ['발탄(하드)','https://ark.bynn.kr/assets/lostark/commander0.png'];
+raidsDic[8] = ['발탄(헬)','https://ark.bynn.kr/assets/lostark/commander0.png'];
+//비아
+raidsDic[3] = ['비아키스(노말)','https://ark.bynn.kr/assets/lostark/commander1.png'];
+raidsDic[4] = ['비아키스(하드)','https://ark.bynn.kr/assets/lostark/commander1.png'];
+raidsDic[9] = ['비아키스(헬)','https://ark.bynn.kr/assets/lostark/commander1.png'];
+//쿠크
+raidsDic[5] = ['쿠크세이튼(노말)','https://ark.bynn.kr/assets/lostark/commander2.png'];
+raidsDic[10] = ['쿠크세이튼(헬)','https://ark.bynn.kr/assets/lostark/commander2.png'];
+//아브
+raidsDic[6] = ['아브렐슈드(노말)','https://ark.bynn.kr/assets/lostark/commander3.png'];
+raidsDic[7] = ['아브렐슈드(하드)','https://ark.bynn.kr/assets/lostark/commander3.png'];
+
+
+function makeEmbed(type, title, category, user, userAvatar) {
+	if (type == 1) {
+		categoryName = contentsDic[category][0]
+		categoryImage = contentsDic[category][1]
+	}
+	else if (type == 2) {
+		categoryName = raidsDic[category][0]
+		categoryImage = raidsDic[category][1]
+	}
+	const exampleEmbed = new MessageEmbed()
+		.setTitle(title)
+		.setAuthor({
+			name: categoryName,
+			iconURL: categoryImage
+		})
+		.setColor("#e3c7ff")
+		.setDescription("내용")
+		.setFooter({
+			text: user,
+			iconURL: userAvatar
+		})
+		.setTimestamp()
+
+	return exampleEmbed
+}
+
+const smallParty = new MessageActionRow()
+	.addComponents(
+		new MessageButton()
+			.setCustomId('attendFirstDps')
+			.setLabel('1팟딜러')
+			.setStyle('PRIMARY')
+	)
+	.addComponents(
+		new MessageButton()
+			.setCustomId('attendFirstSup')
+			.setLabel('1팟서폿')
+			.setStyle('SUCCESS')
+	);
+
+const bigParty = new MessageActionRow()
+	.addComponents(
+		new MessageButton()
+			.setCustomId('attendFirstDps')
+			.setLabel('1팟딜러')
+			.setStyle('PRIMARY')
+	)
+	.addComponents(
+		new MessageButton()
+			.setCustomId('attendFirstSup')
+			.setLabel('1팟서폿')
+			.setStyle('SUCCESS')
+	)
+	.addComponents(
+		new MessageButton()
+			.setCustomId('attendSecondDps')
+			.setLabel('2팟딜러')
+			.setStyle('PRIMARY')
+	)
+	.addComponents(
+		new MessageButton()
+			.setCustomId('attendSecondSup')
+			.setLabel('2팟서폿')
+			.setStyle('SUCCESS')
+	);
+
+const redButtons = new MessageActionRow()
+	.addComponents(
+		new MessageButton()
+			.setCustomId('cancel')
+			.setLabel('취소하기')
+			.setStyle('DANGER')
+	)
+	.addComponents(
+		new MessageButton()
+			.setCustomId('delete')
+			.setLabel('삭제하기')
+			.setStyle('DANGER')
+	);
+
+const selectClass = new MessageActionRow()
+	.addComponents(
+		new MessageSelectMenu()
+			.setCustomId('selectClass')
+			.setPlaceholder('참여할 직업을 선택해주세요')
+			.addOptions([
+				{
+					label: '버서커',
+					value: '1',
+				},
+				{
+					label: '디스트로이어',
+					value: '2',
+				},
+				{
+					label: '워로드',
+					value: '3',
+				},
+				{
+					label: '홀리나이트',
+					value: '4',
+				},
+				{
+					label: '배틀마스터',
+					value: '5',
+				},
+				{
+					label: '인파이터',
+					value: '6',
+				},
+				{
+					label: '기공사',
+					value: '7',
+				},
+				{
+					label: '창술사',
+					value: '8',
+				},
+				{
+					label: '스트라이커',
+					value: '9',
+				},
+				{
+					label: '데빌헌터',
+					value: '10',
+				},
+				{
+					label: '블래스터',
+					value: '11',
+				},
+				{
+					label: '호크아이',
+					value: '12',
+				},
+				{
+					label: '스카우터',
+					value: '13',
+				},
+				{
+					label: '건슬링어',
+					value: '14',
+				},
+				{
+					label: '아르카나',
+					value: '15',
+				},
+				{
+					label: '서머너',
+					value: '16',
+				},
+				{
+					label: '바드',
+					value: '17',
+				},
+				{
+					label: '소서리스',
+					value: '18',
+				},
+				{
+					label: '데모닉',
+					value: '19',
+				},
+				{
+					label: '블레이드',
+					value: '20',
+				},
+				{
+					label: '리퍼',
+					value: '21',
+				},
+				{
+					label: '도화가',
+					value: '22',
+				},
+			]),
+	);
+
+//슬래시커맨드 인터렉션
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
-	console.log(interaction);
-
 	if (interaction.commandName === '모집') {
-		await interaction.reply('처리 완료');
+		const pCate = interaction.options.getString('분류');
+		const pNum = interaction.options.getString('모집인원');
+		const pTitle = interaction.options.getString('제목');
+		let pType = smallParty
+		if (pNum == 1) {
+			pType = smallParty
+		} else {
+			pType = bigParty
+		}
+		await interaction.reply({
+			embeds: [makeEmbed(1, pTitle, pCate, interaction.user.username, interaction.user.avatarURL())],
+			components: [pType, redButtons]
+		});
 	}
 	
 	if (interaction.commandName === '군단장') {
-		await interaction.reply('처리 완료');
+		const pCate = interaction.options.getString('분류');
+		const pTitle = interaction.options.getString('제목');
+		let pType = smallParty
+		console.log(pCate)
+		if (['5','10'].includes(pCate)) {
+			pType = smallParty
+		}
+		else {
+			pType = bigParty
+		}
+		await interaction.reply({
+			embeds: [ makeEmbed(2,pTitle,pCate,interaction.user.username,interaction.user.avatarURL()) ],
+			components: [pType, redButtons]
+		});
 	}
+});
+
+//버튼 인터렉션
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isButton()) return;
+	console.log()
+	console.log()
+	if (interaction.customId == "attendFirstDps"){
+		await interaction.reply({
+			ephemeral: true, components: [selectClass]
+		});
+	}
+	else if (interaction.customId == "delete"){
+		if(interaction.user.username == interaction.message.embeds[0].footer.text){
+			interaction.message.delete();
+		}
+		else {
+			await interaction.reply({ephemeral: true, content:"본인이 작성한 글만 삭제할 수 있습니다."})
+		}
+		
+	}
+	else {
+		await interaction.reply("뭔가 이상함")
+	}
+
 });
 
 client.login(token);
